4ee8dfb7a11e21518d4ed17a16b7194556cd5085
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-10-01 16:32:59 +0530

    Setup Server and basic routes

--- routes/route.js
+++ routes/route.js
@@ -1,1 +1,9 @@
-random
+const express = require('express');
+const router = express.Router();
+const {home, login, register} = require("../controllers/userController")
+
+router.get('/', home);
+router.get('/login', login);
+router.get('/register', register);
+
+module.exports = router;
