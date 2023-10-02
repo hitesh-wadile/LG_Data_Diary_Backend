81d33c4ddd921b44f596c0a5dfaec3746f2ae74b
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2023-10-02 23:19:55 +0530

    Configured Database, Cloudinary & mailSender

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require('./routes/route');
 const bodyParser = require("body-parser");
+const database = require("./config/database");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes );
+
+database.connect();
 
 app.listen(process.env.PORT, (req, res)=>{
     console.log("Server Listening on Port 3000")
